be591435c7a2ba6d2ec0c979ae30396769532dc8
0STG0T <ostgot116@gmail.com>
2024-08-19 22:58:32 +0300

    updated train

--- universal_predictor/pyfiles/train.py
+++ universal_predictor/pyfiles/train.py
@@ -1,19 +1,16 @@
-from utils.utils import *
+import os
+import time
+import warnings
+from datetime import datetime
 import pandas as pd
 import numpy as np
-from parsing.hist_parsing import KlineDataRetriever
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, precision_recall_fscore_support
 from catboost import CatBoostClassifier
-import plotly.graph_objects as go
-from datetime import datetime
-import os
-import warnings
-import time
+from parsing.hist_parsing import KlineDataRetriever
+from utils.utils import *
 
 warnings.filterwarnings('ignore')
-
-print(os.listdir('./'))
 
 # Load configuration from environment variables
 symbol = os.getenv('SYMBOL')
@@ -24,6 +21,7 @@ tss_test_size = int(os.getenv('TSS_TEST_SIZE'))
 target_window_size = int(os.getenv('TARGET_WINDOW_SIZE'))
 actual_test_size = int(os.getenv('TEST_SIZE'))
 
+# Parsing parameters for data retrieval
 parsing_params = {
     'category': 'linear',
     'symbol': symbol,
@@ -34,15 +32,17 @@ parsing_params = {
 }
 
 def parse_and_preprocess_data(parsing_params, prediction=False) -> pd.DataFrame:
-    retriever = KlineDataRetriever(category=parsing_params['category'], symbol=parsing_params['symbol'], 
-                                interval=parsing_params['interval'], testnet=parsing_params['testnet'])
+    retriever = KlineDataRetriever(
+        category=parsing_params['category'], 
+        symbol=parsing_params['symbol'], 
+        interval=parsing_params['interval'], 
+        testnet=parsing_params['testnet']
+    )
     
-    if prediction:
-        data = retriever.fetch_last_data(parsing_params['end_date'])
-        
-    else:
-        data = retriever.fetch_data(parsing_params['start_date'], parsing_params['end_date'])
+    # Fetch data based on mode (prediction or training)
+    data = retriever.fetch_last_data(parsing_params['end_date']) if prediction else retriever.fetch_data(parsing_params['start_date'], parsing_params['end_date'])
     
+    # Data preprocessing and feature engineering
     preprocessed_data = preprocess_data(data)
     data_with_indicators = add_technical_indicators(preprocessed_data)
     data_with_holidays = add_holidays(data_with_indicators)
@@ -54,90 +54,105 @@ def parse_and_preprocess_data(parsing_params, prediction=False) -> pd.DataFrame:
     return final_data
 
 def preprocess_for_training(final_data: pd.DataFrame):
-
+    # Split data into training and testing sets
     train_data = final_data[:-actual_test_size]
     test_data = final_data[-actual_test_size:]
     
+    # Create lagged features
     train_data = create_n_features(train_data, n=n_back_features)
     test_data = create_n_features(test_data, n=n_back_features)
     
+    # Separate features and target variables
     X, y = train_data.drop(columns=['TARGET', 'DATETIME']), train_data['TARGET']
     X_test, y_test = test_data.drop(columns=['TARGET', 'DATETIME']), test_data['TARGET']
     
-    categorical_starts =[
-        'TARGET',
-        'BULLISH',
-        'BEARISH',
-        'OVERBOUGHT_RSI',
-        'OVERSOLD_RSI',
-        'MACD_BULLISH',
-        'MACD_BEARISH',
-        'BB_BANDWIDTH_HIGH',
-        'BB_BANDWIDTH_LOW',
-        'STOCH_OVERBOUGHT',
-        'STOCH_OVERSOLD',
-        'TSI_BULLISH',
-        'TSI_BEARISH',
-        'UO_OVERBOUGHT',
-        'UO_OVERSOLD'
+    # Identify categorical features
+    categorical_starts = [
+        'TARGET', 'BULLISH', 'BEARISH', 'OVERBOUGHT_RSI', 'OVERSOLD_RSI',
+        'MACD_BULLISH', 'MACD_BEARISH', 'BB_BANDWIDTH_HIGH', 'BB_BANDWIDTH_LOW',
+        'STOCH_OVERBOUGHT', 'STOCH_OVERSOLD', 'TSI_BULLISH', 'TSI_BEARISH',
+        'UO_OVERBOUGHT', 'UO_OVERSOLD'
     ]
-
-    cat_features = []
-
-    for cs in categorical_starts:
-        cat_features += [x if x.startswith(cs) else '' for x in X.columns]
-
-    while '' in cat_features:
-        cat_features.remove('')
+    cat_features = [col for cs in categorical_starts for col in X.columns if col.startswith(cs)]
     
     return X, y, cat_features, X_test, y_test
+
+def evaluate_model_performance(model, X_val, y_val, dataset_name):
+    y_pred = model.predict(X_val).flatten()
+    
+    # Calculate and print evaluation metrics
+    precision, recall, f1, _ = precision_recall_fscore_support(y_val, y_pred, average=None)
+    f1_macro = f1_score(y_val, y_pred, average="macro")
+    
+    print(f'\nPerformance on {dataset_name}:')
+    print(f'F1-macro: {f1_macro}')
+    print(f'Precision per class: {precision}')
+    print(f'Recall per class: {recall}')
+    print(f'F1 per class: {f1}')
+    return f1_macro
 
 def train_model(X, y, cat_features, X_test, y_test):
     kf = TimeSeriesSplit(n_splits=tss_n_splits, test_size=tss_test_size)
     
     models_list = []
     
-    print(X.shape)
-
+    print(f"Training on data of shape: {X.shape}")
+    
     for i, (train_idx, val_idx) in enumerate(kf.split(X, y)):
         X_train, y_train = X.iloc[train_idx], y.iloc[train_idx]
         X_val, y_val = X.iloc[val_idx], y.iloc[val_idx]
         
-        clf = CatBoostClassifier(10000, l2_leaf_reg=1.5, bootstrap_type='Bayesian', early_stopping_rounds=100, use_best_model=True,
-                                colsample_bylevel=0.95, random_state=42, posterior_sampling=True,
-                                leaf_estimation_method='Newton', cat_features=cat_features, auto_class_weights='Balanced')
+        clf = CatBoostClassifier(
+            iterations=10000, 
+            l2_leaf_reg=1.5, 
+            bootstrap_type='Bayesian', 
+            early_stopping_rounds=100, 
+            use_best_model=True,
+            colsample_bylevel=0.95, 
+            random_state=42, 
+            posterior_sampling=True,
+            leaf_estimation_method='Newton', 
+            cat_features=cat_features, 
+            auto_class_weights='Balanced'
+        )
         
         clf.fit(X_train, y_train, eval_set=(X_val, y_val), verbose=100)
         
-        score = f1_score(y_val, clf.predict(X_val).flatten(), average="weighted")
+        # Evaluate performance on the validation set
+        f1_macro_val = evaluate_model_performance(clf, X_val, y_val, f"Validation Fold {i}")
         
-        print(f'FOLD {i} F1: {score}')
+        models_list.append((clf, f1_macro_val))
         
-        models_list.append((clf, score))
-        
-    model = max(models_list, key=lambda x: x[1])[0]
+    # Select the best model based on validation F1-macro
+    best_model = max(models_list, key=lambda x: x[1])[0]
     
-    # Evaluating on the test data
-    score = f1_score(y_test, clf.predict(X_test).flatten(), average="weighted")
+    # Evaluate the best model on the test data
+    evaluate_model_performance(best_model, X_test, y_test, "Test Set")
     
-    print(f'Test data F1: {score}')
-    
-    return model
+    return best_model
 
 def train_and_save() -> str:
-    print('Starting parsing!')
-    t1 = time.time()
-    final_data = parse_and_preprocess_data(parsing_params=parsing_params)
-    print('Parsed in', time.time() - t1, 'seconds')
+    print('Starting data parsing and preprocessing...')
+    start_time = time.time()
     
+    final_data = parse_and_preprocess_data(parsing_params=parsing_params)
+    
+    elapsed_time = time.time() - start_time
+    print(f'Data parsed and preprocessed in {elapsed_time:.2f} seconds')
+    
+    # Prepare data for training
     X, y, cat_features, X_test, y_test = preprocess_for_training(final_data=final_data)
+    
+    # Train the model and evaluate
     model = train_model(X=X, y=y, cat_features=cat_features, X_test=X_test, y_test=y_test)
     
-    model.save_model('./weis/cb')
+    # Save the trained model
+    model_path = './weis/cb'
+    model.save_model(model_path)
     
-    print('Model saved!')
+    print(f'Model saved to {model_path}!')
     
-    return model
+    return model_path
 
 if __name__ == '__main__':
     train_and_save()
